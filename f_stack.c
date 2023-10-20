45a407c1fc153e1e14859185833b3f744c4b1020
Janecanal <janecanal04@gmail.com>
2023-10-19 17:14:59 -0700

    monty project

--- f_stack.c
+++ f_stack.c
@@ -1,11 +1,12 @@
 #include "monty.h"
+
 /**
  * free_stack - Function that frees a the stack
  *
  * @stack: The stack to free
- *
  * Return: 0
  */
+
 void free_stack(stack_t *stack)
 {
 	stack_t *next_node;
